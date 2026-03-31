64dcd489cf7ceaa84ff4b466e443cd33f8884755
Konstantin Tkachenko <kirkos1990@yandex.ru>
2026-03-31 19:35:04 +0300

    Welcome message was changed

--- src/main/java/com/github/kostapuz/Main.java
+++ src/main/java/com/github/kostapuz/Main.java
@@ -2,7 +2,7 @@ package com.github.kostapuz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello world!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
